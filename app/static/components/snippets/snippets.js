33e0d872d62a1d9b94e5fe922955aaf99520e4fc
John <johnmarksjr@gmail.com>
2014-10-24 17:27:42 -0600

    Implemented snippetPopup directive

--- app/static/components/snippets/snippets.js
+++ app/static/components/snippets/snippets.js
@@ -3,15 +3,25 @@ viewsModule.controller('SnippetsCtrl', ['$scope',
     $scope.SnippetsCtrlScope = "SnippetsCtrlScope";
 }])
 
-.directive('snippet', function() {
+.directive('snippet', ['$sce', 'snippetBarService', 'oauthLibrary',
+               function($sce,   snippetBarService,   oauth) {
     return {
         restrict: 'E',
         scope: true,
         templateUrl: './static/components/snippets/snippets.html',
-        controller: function($scope, $sce, $element, $attrs, oauthLibrary, snippetBarService) {
-            $scope.SnippetDirectiveScope = "SnippetDirectiveScope";
-            $scope.snippetPopupVisible = false;
-            $scope.getTrustedHtml = function(htmlStr) {
+        link: function(scope, element, attrs) {
+            console.log("In snippet directive link");
+            scope.snippetPopupVisible = false;
+            scope.isPublicSnippet = function(snippetAccess) {
+                return snippetAccess;
+            };
+            scope.isSnippetOwnedByCurrentUser = function(creatorId) {
+                return oauth.isAuthenticated() ? true : false;
+            };
+            scope.layout = function() {
+                return snippetBarService.snippetLayout;
+            };
+            scope.getTrustedHtml = function(htmlStr) {
                 var layout = snippetBarService.snippetLayout;
                 if (layout === 'titlesonly') {
                     return "";
@@ -19,23 +29,19 @@ viewsModule.controller('SnippetsCtrl', ['$scope',
                     return $sce.trustAsHtml(htmlStr);
                 }
             };
-            $scope.isPublicSnippet = function(snippetAccess) {
-                return snippetAccess;
+            scope.showSnippetPopup = function() {
+                scope.snippetPopupVisible = true;
             };
-            $scope.isSnippetOwnedByCurrentUser = function(creatorId) {
-                return oauthLibrary.isAuthenticated() ? true : false;
+            scope.hideSnippetPopup = function() {
+                scope.snippetPopupVisible = false;
             };
-            $scope.layout = function() {
-                return snippetBarService.snippetLayout;
-            };
-            $scope.showSnippetPopup = function() {
-                $scope.snippetPopupVisible = true;
-            };
-            $scope.hideSnippetPopup = function() {
-                $scope.snippetPopupVisible = false;
-            };
-        },
-        link: function(scope, element, attrs) {
         }
+    }
+}])
+
+.directive('snippetPopup', function() {
+    return {
+        restrict: 'E',
+        templateUrl: './static/components/snippets/snippetPopup.html'
     }
 });
