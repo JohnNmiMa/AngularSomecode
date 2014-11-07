49a1a3e488603f65147075a001338cfceb505add
John <johnmarksjr@gmail.com>
2014-11-07 14:44:17 -0700

    Cleanup up topicPanel UI

--- app/static/signedout/signedout.js
+++ app/static/signedout/signedout.js
@@ -1,6 +1,7 @@
 viewsModule.controller('SignedoutCtrl', ['$scope', '$sce', 'snippetService',
                                  function($scope,   $sce,   snippetService) {
     $scope.SignedOutCtrlScope = "SignedOutCtrlScope";
+
     $scope.snippets = {};
     $scope.$on('updateSnippets', function(event) {
         $scope.snippets = snippetService.snippets;
--- app/static/user/user.js
+++ app/static/user/user.js
@@ -1,5 +1,7 @@
 viewsModule.controller('UserCtrl', ['$scope', 'topics', 'snippetService',
                             function($scope,   topics,   snippetService) {
+    $scope.UserCtrlScope = "UserCtrlScope";
+
     $scope.snippets = {};
     snippetService.setTopics(topics, $scope);
     $scope.$on('updateSnippets', function(event) {
