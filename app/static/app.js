b4e8977c86751089ce41bad78a75b1e1c0390b33
John <johnmarksjr@gmail.com>
2014-11-20 22:09:10 -0700

    Implemented gulp builds
    Mostly working. Can’t search for some reason.

--- app/static/app.js
+++ app/static/app.js
@@ -1,4 +1,4 @@
-var someCodeApp = angular.module('SomeCodeApp', ['someCodeViews', 'ngRoute', 'ui.bootstrap'])
+var someCodeApp = angular.module('SomeCodeApp', ['someCodeViews', 'ngRoute'])
 
 .run(['$rootScope', '$location', '$route', '$timeout', function ($rootScope, $location, $route, $timeout) {
     $rootScope.$on('$routeChangeStart', function () {
--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,63 @@
-// Include gulp
-var gulp = require('gulp'); 
-var connect = require('gulp-connect');
+//inside of gulpfile.js
+var gulp = require('gulp');
+var shell = require('gulp-shell');
 var usemin = require('gulp-usemin');
-var minifyHtml = require('gulp-minify-html');
 var minifyCss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var rev = require('gulp-rev');
-var clean = require('gulp-clean');
 var replace = require('gulp-replace');
-var rename = require('gulp-rename');
 
-gulp.task('copy-html-files', function() {
-    //gulp.src(['./app/**/*.html', './app/**/*.css',
-    //gulp.src(['./app/**/*.html', '!./app/index.html', './app/**/*.css',
-    gulp.src(['./app/**/*.html', '!./app/index.html',
-              './app/bower_components/fontawesome/fonts/*.otf',
-              './app/bower_components/fontawesome/fonts/*.eot',
-              './app/bower_components/fontawesome/fonts/*.svg',
-              './app/bower_components/fontawesome/fonts/*.ttf',
-              './app/bower_components/fontawesome/fonts/*.woff',
-              './app/bower_components/bootstrap/dist/css/bootstrap.css.map',
-              './app/bower_components/bootstrap/dist/fonts/*.eot',
-              './app/bower_components/bootstrap/dist/fonts/*.svg',
-              './app/bower_components/bootstrap/dist/fonts/*.ttf',
-              './app/bower_components/bootstrap/dist/fonts/*.woff',
-              ], {base: './app'})
+gulp.task('flask', shell.task(['source ./venv/bin/activate && python run.py']));
+
+gulp.task('copy-files', function() {
+    gulp.src(['!./app/templates/index.html',
+              './app/**/*.html',
+              './app/static/**/*.html',
+              './app/static/components/**/*.html',
+              './app/static/components/snippetpanel/arrow-down.png',
+              '*.py', './app/*.py', 'requirements.txt',
+              'app.db',
+              './app/static/bower_components/codemirror/mode/**/*.js',
+              './app/static/bower_components/fontawesome/fonts/*.otf',
+              './app/static/bower_components/fontawesome/fonts/*.eot',
+              './app/static/bower_components/fontawesome/fonts/*.svg',
+              './app/static/bower_components/fontawesome/fonts/*.ttf',
+              './app/static/bower_components/fontawesome/fonts/*.woff',
+              './app/static/bower_components/bootstrap/dist/css/bootstrap.css.map',
+              './app/static/bower_components/bootstrap/dist/fonts/*.eot',
+              './app/static/bower_components/bootstrap/dist/fonts/*.svg',
+              './app/static/bower_components/bootstrap/dist/fonts/*.ttf',
+              './app/static/bower_components/bootstrap/dist/fonts/*.woff'
+    ], {base: '.'})
     .pipe(gulp.dest('build/'));
 });
 
-gulp.task('fix-awesome-paths', ['fix-glyphicon-paths'], function() {
-    gulp.src('./build/_assets/combined-*.css')
-    .pipe(replace('../fonts/fontawesome-webfont', '../bower_components/fontawesome/fonts/fontawesome-webfont'))
-    .pipe(gulp.dest('./build/_assets'));
-});
-
-gulp.task('fix-glyphicon-paths', ['usemin'], function() {
-    return gulp.src('./build/_assets/combined-*.css')
-    .pipe(replace('../fonts/glyphicons-halflings', '../bower_components/bootstrap/dist/fonts/glyphicons-halflings'))
-    .pipe(gulp.dest('./build/_assets'));
-});
-
 gulp.task('usemin', function() {
-    return gulp.src('./app/index.html')
+    return gulp.src('app/templates/index.html')
     .pipe(usemin({
         assetsDir: 'app',
         css: [minifyCss(), 'concat', rev()],
-        //js: [uglify({mangle: false}), rev()] // don't mangle names
+        js: [uglify({mangle: false}), rev()] // don't mangle names
         //js: [uglify({preserveComments: 'some'}), rev()] // keep comments that start with !
-        js: [uglify(), rev()]
+        //js: [uglify(), rev()]
     }))
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest('build/app/templates'));
 });
 
-gulp.task('connect', function() {
-    connect.server({
-        root: 'app/'
-    });
+gulp.task('fix-glyphicon-paths', ['usemin'], function() {
+    return gulp.src('./build/app/static/combined-*.css')
+        .pipe(replace('../fonts/glyphicons-halflings', '../static/bower_components/bootstrap/dist/fonts/glyphicons-halflings'))
+        .pipe(gulp.dest('./build/app/static'));
 });
 
-// Default Task
-gulp.task('default', ['connect']);
+gulp.task('fix-awesome-paths', ['fix-glyphicon-paths'], function() {
+    gulp.src('./build/app/static/combined-*.css')
+    .pipe(replace('../fonts/fontawesome-webfont', '../static/bower_components/fontawesome/fonts/fontawesome-webfont'))
+    .pipe(gulp.dest('./build/app/static'));
+});
+
+// Default Task - run the Flask server
+gulp.task('default', ['flask']);
+
 // Build Task - has multiple dependencies that need to run serially
-gulp.task('build', ['copy-html-files', 'usemin', 'fix-awesome-paths', 'fix-glyphicon-paths']);
+gulp.task('build', ['copy-files', 'usemin', 'fix-glyphicon-paths', 'fix-awesome-paths']);
