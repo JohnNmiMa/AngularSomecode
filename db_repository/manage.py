c892662c07f58060f8271d1a162544fcf73d3e16
John <johnmarksjr@gmail.com>
2014-12-23 20:00:18 -0700

    Update the database prep utility di.py
    Tidied up the Welcome instructions.

--- db_repository/manage.py
+++ db_repository/manage.py
@@ -2,4 +2,4 @@
 from migrate.versioning.shell import main
 
 if __name__ == '__main__':
-    main(six='<module 'six' from '/Users/johnmarks/Src/GitRepos/Thinkful/ANG/unit4/somecode-capstone/venv/lib/python2.7/site-packages/six.pyc'>')
+    main(six='<module 'six' from '/usr/local/Cellar/python/2.7.6/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/six.pyc'>')
